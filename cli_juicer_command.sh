f31124f04c3dcea823cef5acbe8d50a854ea53f9
skurscheid <sebastian.kurscheid@anu.edu.au>
2020-05-11 13:52:05 +1000

    minor modification

--- cli_juicer_command.sh
+++ cli_juicer_command.sh
@@ -2,7 +2,7 @@
 
 
 # CPU 
-#~/software/bin/juicer/scripts/juicer.sh -g hg38 -s Arima -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/genome.fa.fai -t 32 -y ~/software/bin/juicer/restriction_sites/hg38_Arima.txt
+~/software/bin/juicer/scripts/juicer.sh -g hg38 -s Arima -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt -t 32 -y ~/software/bin/juicer/restriction_sites/hg38_Arima.txt
 
 # PBS
-~/software/bin/juicer/scripts/juicer.sh -g hg38 -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/genome.fa.fai -y ~/software/bin/juicer/restriction_sites/hg38_Arima.txt -D ~/software/bin/juicer -s Arima
+#~/software/bin/juicer/scripts/juicer.sh -g hg38 -p ~/data/References/Genomes/Homo_sapiens/GRCh38_ensembl84/canonical/chrom_sizes.txt -y ~/software/bin/juicer/restriction_sites/hg38_Arima.txt -D ~/software/bin/juicer -s Arima
